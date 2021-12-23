dd23affdb53677097973e73ab31b980999c6153e
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 10:37:57 +0100

    Creando metodos para recibir los datos de la API

--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -6,8 +6,21 @@ import { Injectable } from '@angular/core';
 })
 
 export class RequestService {
+  private data: any;
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { 
-    
+  async getWorkspace() {
+    this.setData(await this.http.get('http://localhost:3001').toPromise())
+    return this.getData();
   }
+
+  setData(request: any): void {
+    this.data = JSON.stringify(request)
+    this.data = JSON.parse(this.data)
+  }
+
+  getData(): Object {
+    return this.data;
+  }
+
 }
