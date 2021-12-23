143b2d27865d75487f29d4beb71dc7d1d9561721
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 12:28:20 +0100

    Eliminando app.title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'gestor-documental'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gestor-documental');
   });
 
   it('should render title', () => {
