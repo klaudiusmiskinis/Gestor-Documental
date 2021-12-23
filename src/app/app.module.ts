78a07be7ebd50c615ed68c3d0831178706c4adee
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 09:22:26 +0100

    Importando APP_BASE_HREF y Seteando los providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
