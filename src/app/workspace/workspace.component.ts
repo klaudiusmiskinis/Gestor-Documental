17eb30a961c185a080fb1613c4561d3c1022ed8b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 12:03:01 +0100

    Añadiendo metodos que cumplen con el correcto funcionamiento de movernos entre carpetas.

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -7,19 +7,46 @@ import { RequestService } from '../request.service';
 })
 
 export class WorkspaceComponent implements OnInit {
-  public content: any = {}
-  public path: string;
+  public content: any;
+  private path: string;
   constructor(private request: RequestService) { 
-    this.path = 'http://localhost:3001';
+    this.path = 'http://localhost:3001/';
   }
 
   ngOnInit(): void {
-    this.getService();
+    this.getContent(this.getPath());
   }
 
-  async getService() {
-    this.content = await this.request.getWorkspace();
-    console.log(this.content)
+  getPath(): string {
+    return this.path;
+  }
+
+  setPath(path: string): void {
+    this.path = path;
+  }
+
+  getBack() {
+    if (this.getPath().includes('?path=')){
+      if (this.getPath().substring(0, this.getPath().lastIndexOf('/')) === 'http://localhost:3001'){
+        this.setPath(this.getPath().substring(0, this.getPath().lastIndexOf('/')) + '/');
+      } else {
+        this.setPath(this.getPath().substring(0, this.getPath().lastIndexOf('/')));
+      }
+      this.getContent(this.getPath());
+    }
+  }
+
+  async selectedFolder(foldername: string) {
+    if (this.getPath().includes('?path')) {
+      this.setPath(this.getPath() + '/' + foldername)
+    } else {
+      this.setPath(this.getPath() + '?path=' + foldername)
+    }
+    this.getContent(this.getPath())
+  }
+
+  async getContent(path: string) {
+    this.content = await this.request.getWorkspace(path);
   }
 
 }
