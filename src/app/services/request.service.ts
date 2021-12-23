f181a4ffe684b7eaad3b4ee0e7029754a1392eaa
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 14:11:32 +0100

    Añadiendo metodo para eliminar archivo

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { catchError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,9 +9,18 @@ export class RequestService {
   private data: any;
   constructor(private http: HttpClient) {}
 
-  async getWorkspace(path: string) {
+  async getWorkspace(path: string): Promise <Object> {
     this.setData(await this.http.get(path).toPromise())
     return this.getData();
+  }
+
+  async deleteFile(path: string, file: string): Promise <void> {
+    let parameter = '?';
+    if (path.includes('?')) {
+      parameter = '&';
+    }
+    const a = await this.http.delete(path + parameter  +'file=' + file).toPromise()
+    console.log(a);
   }
 
   setData(request: any): void {
