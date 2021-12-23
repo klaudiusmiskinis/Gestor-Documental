5ba0c5f2ee3ab9c43f7b21b95fa317ce4d453fcd
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 09:21:52 +0100

    Añadiendo routes, importando el componente workspace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'workspace', component: WorkspaceComponent, pathMatch: 'full' },
+  { path: '',   redirectTo: '/workspace', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
