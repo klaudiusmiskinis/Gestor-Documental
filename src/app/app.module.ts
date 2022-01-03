e047f10cfc65a020f603bdd756b25012abdde423
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-03 13:33:56 +0100

    Importando Notifier y añadiendo configuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { APP_BASE_HREF } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
 
 /* NG-Material */
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -17,6 +17,48 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 /* NGX-Bootstrap */
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+const customNotifierOptions: NotifierOptions = {
+  position: {
+		horizontal: {
+			position: 'right',
+			distance: 12
+		},
+		vertical: {
+			position: 'bottom',
+			distance: 32,
+			gap: 10
+		}
+	},
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +70,8 @@ import { AccordionModule } from 'ngx-bootstrap/accordion';
     AppRoutingModule,
     HttpClientModule,
     BrowserModule,
+    /* NG-notifier */
+    NotifierModule.withConfig(customNotifierOptions),
 
     /* NGX-Bootstrap */
     AccordionModule.forRoot(),
