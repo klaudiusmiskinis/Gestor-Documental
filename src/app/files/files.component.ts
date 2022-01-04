b008ea393b705e38d5da650cf9322ac4ebef5998
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-04 21:05:21 +0100

    Generando componente files y añadiendo funcionamiento

---
+++ src/app/files/files.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RequestService } from '../services/request.service';
+
+@Component({
+  selector: 'files',
+  templateUrl: './files.component.html',
+  styleUrls: ['./files.component.css']
+})
+
+export class FilesComponent implements OnInit {
+
+  constructor(private request: RequestService) { }
+
+  @Input() files: any;
+  @Input() path: string;
+  @Output() fileEvent = new EventEmitter<Object>();
+
+  ngOnInit(): void {
+  }
+
+  
+
+  getFile(file: string): string {
+    return this.request.getFile(this.path, file);
+  };
+
+ fileEmitter (type: string,file: string) {
+    this.fileEvent.emit({type: type, file:file});
+  }
+}
