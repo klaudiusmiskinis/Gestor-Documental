6a32ecfa47c58a47abea899b8fd580c9dc1aba76
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-07 11:50:59 +0100

    Creando clase FileInfo

---
+++ src/app/models/fileInfo.ts
@@ -0,0 +1,49 @@
+export class FileInfo {
+    public exists: Boolean;
+    public name?: string;
+    public size?: string;
+    public extension?: string;
+
+    constructor(exists, name?, size?) {
+        this.exists = exists;
+        if (exists) {
+            this.name = name;
+            this.calculateSize(size);
+            this.getExtension();
+        }
+    }
+
+    public getExtension(): void {
+        if (this.name) {
+            this.extension = this.name.split('.')[this.name.split('.').length - 1]
+        }
+    }
+
+    public calculateSize(size: number): void {
+        let counterLoop = 0;
+        do {  
+           size  /= 1024;
+           size.toFixed(2);
+           counterLoop++;
+        } while (size > 1024); 
+        this.size = size.toFixed(2) + this.getUnitOfInformation(counterLoop);
+    }
+
+    public getUnitOfInformation(counter: number): string {
+        switch (counter) {
+            case 0:
+                return ' B';
+            case 1:
+                return ' KB';
+            case 2:
+                return ' MB';
+            case 3:
+                return ' GB';
+            case 4:
+                return ' TB';
+            default:
+                return ' KB'
+        }
+    }
+
+}
