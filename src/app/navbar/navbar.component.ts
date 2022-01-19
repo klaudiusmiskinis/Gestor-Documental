88de96b50165afdd65bd9daa6cd349f30e4a2196
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-19 13:52:47 +0100

    Eliminando servicio de SidebarToggle

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { SidebarToggleService } from '../services/sidebar-toggle.service';
 
 @Component({
   selector: 'navbar',
@@ -9,19 +8,13 @@ export class NavbarComponent implements OnInit {
 
   public sidebarStatus: Boolean;
 
-  constructor(private evtSvc: SidebarToggleService) { 
+  constructor() { 
     this.sidebarStatus = false;
   }
 
   @Input() path: string;
 
   ngOnInit(): void {
-    this.path
-  }
-
-  eventSidebar() {
-    this.sidebarStatus = !this.sidebarStatus;
-    this.evtSvc.childEventEmitter(this.sidebarStatus)
   }
 
 }
