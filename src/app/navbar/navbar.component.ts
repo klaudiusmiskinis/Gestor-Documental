67054c32a91835c0655751eadfaa5bf9263f9444
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-01 12:09:12 +0100

    Añadiendo funcionamiento de la ruta que se quiera que se muestre.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {
 
-  public sidebarStatus: Boolean;
+export class NavbarComponent implements OnChanges {
 
-  constructor() {}
+  public renderedPath: string;
 
   @Input() path: string;
+
+  ngOnChanges(): void {
+    this.setupPath();
+  }
+
+  setupPath(): void{
+    this.renderedPath = this.path;
+    if (this.renderedPath.charAt(this.renderedPath.length - 1) === '/') {
+      this.renderedPath = 'Home';
+    } else {
+      this.renderedPath = this.renderedPath.split('/?path=')[this.renderedPath.split('/?path=').length - 1];
+      this.renderedPath = 'Home ➤ ' + this.renderedPath.split('/').join(' ➤ ');
+    }
+  }
 }
