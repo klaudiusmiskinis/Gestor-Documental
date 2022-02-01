f02f29d7a3e0ab6489bb20035d9ce9da9025e4e4
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-01 12:24:51 +0100

    Renderizando componente Admin en la ruta /admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 
 const routes: Routes = [
-  { path: 'admin', component: WorkspaceComponent, pathMatch: 'full' },
+  { path: 'admin', component: AdminComponent, pathMatch: 'full' },
   { path: 'workspace', component: WorkspaceComponent, pathMatch: 'full' },
-  { path: '',   redirectTo: '/workspace', pathMatch: 'full' },
+  { path: '**',   redirectTo: '/workspace', pathMatch: 'full' },
 ];
 
 @NgModule({
