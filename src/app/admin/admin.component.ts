f1379fd87a6311ae1cf5f5d39a11027eb37516fe
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-20 10:42:17 +0200

    Añadiendo diferentes filtros y creando metodos para manejar el sizeColumnsToFit

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ColDef, GridReadyEvent } from 'ag-grid-community';
+import { ColDef, GridOptions } from 'ag-grid-community';
 import { RequestService } from '../services/request.service';
+import { localeEs } from '../../assets/locale.es';
 
 @Component({
   selector: 'app-admin',
@@ -10,28 +11,41 @@ import { RequestService } from '../services/request.service';
 export class AdminComponent implements OnInit {
 
   public columnas: ColDef[] = [
-    { headerName: "ID", filter: 'agNumberColumnFilter', sortable: true, field: "id" },
-    { headerName: 'Nombre', filter: true, sortable: true, field: "name" },
-    { headerName: 'Ruta', filter: true, sortable: true, field: "path" },
-    { headerName: 'Ultima versión', sortable: true, field: "isLastVersion" },
-    { headerName: 'Fecha Creado', filter: 'agDateColumnFilter', sortable: true, field: "createdDate" },
-    { headerName: 'ID Padre', filter: true, sortable: true, field: "idParent" },
-    { headerName: 'Eliminado', filter: true, sortable: true, field: "isRemoved" },
-    { headerName: 'Fecha Eliminado', filter: 'agDateColumnFilter', sortable: true, field: "removedDate" },
-    { headerName: 'Motivo', filter: true, sortable: true, field: "reason" },
+    { headerName: "ID", filter: 'agNumberColumnFilter', sortable: true, field: "id", resizable: true },
+    { headerName: 'Nombre', filter: 'agTextColumnFilter', sortable: true, field: "name", resizable: true },
+    { headerName: 'Ruta', filter: 'agTextColumnFilter', sortable: true, field: "path", resizable: true },
+    { headerName: 'Ultima versión', filter: 'agNumberColumnFilter', sortable: true, field: "isLastVersion" },
+    { headerName: 'Fecha Creado', filter: 'agDateColumnFilter', sortable: true, field: "createdDate", resizable: true },
+    { headerName: 'ID Padre', filter: 'agNumberColumnFilter', sortable: true, field: "idParent", resizable: true },
+    { headerName: 'Eliminado', filter: 'agDateColumnFilter', sortable: true, field: "isRemoved", resizable: true },
+    { headerName: 'Fecha Eliminado', filter: 'agDateColumnFilter', sortable: true, field: "removedDate", resizable: true },
+    { headerName: 'Motivo', filter: 'agTextColumnFilter', sortable: true, field: "reason", resizable: true },
   ];
   public datos: any
   public gridApi: any;
+  public gridOptions = {};
 
   constructor(private request: RequestService) { }
 
   async ngOnInit(): Promise<void> {
     this.datos = await this.request.getAllFiles()
+    this.gridOptions = {
+      localeTextFunc: (key: string, defaultValue: string) => localeEs[key] || defaultValue
+    }
   }
 
   onGridReady(params: any) {
+    window.addEventListener('resize', function () {
+      setTimeout(function () {
+        params.api.sizeColumnsToFit();
+      });
+    });
     this.gridApi = params.api;
     this.gridApi.setDomLayout('autoHeight');
+    this.restartSize();
+  }
+
+  restartSize() {
     this.gridApi.sizeColumnsToFit();
   }
 }
