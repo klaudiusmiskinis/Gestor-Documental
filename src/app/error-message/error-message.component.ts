8f9479f88c305d1fd34d69cc8a0abfe9604db77e
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-27 10:55:15 +0200

    Creando componente para errores dinamicos

---
+++ src/app/error-message/error-message.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { fadeInError } from '../config/animations.config';
+
+@Component({
+  selector: 'error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.css'],
+  animations: [fadeInError]
+})
+export class ErrorMessageComponent implements OnInit {
+
+  @Input() ctrl: FormControl | any;
+
+  ERROR_MESSAGE = {
+    required: () => `El campo es obligatorio.`,
+    minlength: (par) => `Mínimo ${par.requiredLength} caracteres.`,
+    maxlength: (par) => `Máximo ${par.requiredLength} caracteres.`,
+    pattern: () => `No se permiten cáracteres especiales.`,
+    nameExists: () => `Este nombre ya existe.`
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  shouldShowErrors(): boolean {
+    return this.ctrl && this.ctrl.errors;
+  }
+
+  listOfErrors(): string[] {
+    return Object.keys(this.ctrl.errors).map(
+      err => this.ERROR_MESSAGE[err](this.ctrl.getError(err))
+    );
+  }
+
+}
