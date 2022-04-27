7a264c39a9f0bd444c7c8a83acbf13d1bfa03794
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-27 13:53:10 +0200

    Creando formulario y metodo login

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,36 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { fadeInError } from '../config/animations.config';
 declare var $: any;
 
 @Component({
   selector: 'navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  animations: [fadeInError]
 })
 
 export class NavbarComponent implements OnChanges {
 
   public renderedPath: string;
+  public loginForm: FormGroup;
 
   @Input() path: string;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+        Validators.pattern('^[A-zÀ-ú]*$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+      ])
+    })
+  }
 
   ngOnChanges(): void {
     this.setupPath();
@@ -18,6 +38,10 @@ export class NavbarComponent implements OnChanges {
 
   modal(id: string, state: string): void {
     $('#' + id).modal(state);
+  }
+
+  login() {
+    console.log('Login');
   }
 
   setupPath(): void {
