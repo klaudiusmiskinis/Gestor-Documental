f4a5906a3c3d9ab371b0846f54156e06363e168d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-28 10:41:52 +0200

    Eliminando saltos de linea innecesarios

--- src/app/validators/CustomValidators.ts
+++ src/app/validators/CustomValidators.ts
@@ -1,13 +1,10 @@
 import { AbstractControl } from "@angular/forms";
 
-export class CustomValidator{
+export class CustomValidator {
     static numeric(control: AbstractControl) {
-      let val = control.value;
-  
-      if (val === null || val === '') return null;
-  
-      if (!val.toString().match(/^[0-9]+(\.?[0-9]+)?$/)) return { 'invalidNumber': true };
-  
-      return null;
+        let val = control.value;
+        if (val === null || val === '') return null;
+        if (!val.toString().match(/^[0-9]+(\.?[0-9]+)?$/)) return { 'invalidNumber': true };
+        return null;
     }
-  }
+}
