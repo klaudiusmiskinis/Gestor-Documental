d1989335525a45a37148a2f4916eeb8b0599c315
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-28 13:10:04 +0200

    Creando validadores globales

--- src/app/validators/CustomValidators.ts
+++ src/app/validators/CustomValidators.ts
@@ -7,4 +7,31 @@ export class CustomValidator {
         if (!val.toString().match(/^[0-9]+(\.?[0-9]+)?$/)) return { 'invalidNumber': true };
         return null;
     }
+
+    static dateValidator(control: AbstractControl) {
+        let today: string | Date = new Date();
+        today = today.toISOString().slice(0, -14);
+        if (control.value < '1900-1-1') {
+            return { preOld: true };
+        } else if (control.value > today) {
+            return { postToday: true };
+        }
+        return null;
+    }
+
+    static hasExtension(control: AbstractControl) {
+        const value = control.value;
+        if (value.includes('.')) {
+            return { 'hasExtension': true }
+        }
+        return null;
+    }
+
+    static lastSlash(control: AbstractControl) {
+        const value = control.value;
+        if (value.slice(-1) !== '/') {
+            return { 'lastSlash': true }
+        }
+        return null;
+    }
 }
