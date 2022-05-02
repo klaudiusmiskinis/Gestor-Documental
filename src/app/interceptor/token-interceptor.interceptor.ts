5e4e8a70b449043b456b94d3a4d4ea27de511df1
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-02 12:23:50 +0200

    Generando y dando funcionalidad al interceptor de token

---
+++ src/app/interceptor/token-interceptor.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
+
+@Injectable()
+export class TokenInterceptorInterceptor implements HttpInterceptor {
+
+  constructor(private token: TokenService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.token.isSaved()) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer' + this.token.getSavedToken()
+        }
+      })
+    }
+    return next.handle(request);
+  }
+}
