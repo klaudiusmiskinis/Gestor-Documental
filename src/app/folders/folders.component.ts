1a72e076438aa985f4e2cccbd751414754390dc4
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-03 12:33:57 +0200

    Eliminando import de followCursor y eliminando su uso, tambien se ha añadido el atributo isAdmin

--- src/app/folders/folders.component.ts
+++ src/app/folders/folders.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output, } from '@angular/core';
 import { fadeIn, slideDownHideUp, slideIn } from '../config/animations.config';
-import { followCursor } from 'tippy.js';
 
 @Component({
   selector: 'folders',
@@ -13,6 +12,7 @@ export class FoldersComponent implements OnInit {
   public tooltipLeft: object;
   public tooltipBottom: object;
   public menuStatus: boolean;
+  @Input() isAdmin: boolean;
   @Input() expanded: boolean;
   @Input() folders: any;
   @Output() folderEvent = new EventEmitter<Object>();
@@ -24,24 +24,18 @@ export class FoldersComponent implements OnInit {
       placement: 'right',
       animation: 'fade',
       delay: [500, 0],
-      followCursor: true,
-      plugins: [followCursor],
     }
     this.tooltipLeft = {
       arrow: true,
       placement: 'left',
       animation: 'fade',
       delay: [500, 0],
-      followCursor: true,
-      plugins: [followCursor],
     }
     this.tooltipBottom = {
       arrow: true,
       placement: 'bottom',
       animation: 'fade',
       delay: [500, 0],
-      followCursor: true,
-      plugins: [followCursor],
     }
   }
 
