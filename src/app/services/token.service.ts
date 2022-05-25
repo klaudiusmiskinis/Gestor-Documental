53c555950fae37790a84bf6496c48f093e4ccf60
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-25 13:36:48 +0200

    Añadiendo comentarios

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -4,17 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
-
   public token: string
 
   constructor() {
     this.initializingToken();
   }
 
-  getToken() {
+  /**
+   * Devuelve el atributo token
+   * @returns string
+   */
+  getToken(): string {
     return this.token;
   }
 
+  /**
+   * Devuelve el token del localStorage
+   * @returns string
+   */
   getSavedToken() {
     if (this.isSaved()) {
       return localStorage.getItem('Bearer');
@@ -23,28 +30,45 @@ export class TokenService {
     }
   }
 
-  setToken(token) {
+  /**
+   * Asigna el parametro como nuevo valor al atributo token.
+   * @param token string
+   */
+  setToken(token: string) {
     if (!token) throw 'You need to provide a token';
     if (typeof (token) !== 'string') throw 'Type must be string';
     this.token = token;
   }
 
+  /**
+   * Guarda el token en localStorage
+   */
   saveToken() {
     localStorage.setItem('Bearer', this.token);
   }
 
-  isSaved() {
+  /**
+   * Comprueba si el token esta en localStorage
+   * @returns boolean
+   */
+  isSaved(): boolean {
     if (!localStorage.getItem('Bearer')) return false;
     return true;
   }
 
+  /**
+   * Elimina el token del localStorage
+   */
   removeToken() {
     localStorage.removeItem('Bearer')
   }
 
+  /**
+   * Inicializa el token en caso de que este guardado
+   */
   initializingToken() {
     if (this.isSaved()) {
-      const tokenSaved = this.getSavedToken();
+      const tokenSaved: string | any = this.getSavedToken();
       this.setToken(tokenSaved)
     }
   }
